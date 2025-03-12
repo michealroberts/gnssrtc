d5b27227651b90778b0ce0d06efd190b24f0c4fe
michealroberts <michael@observerly.com>
2025-03-12 17:47:59 +0000

    chore: bump package version to 0.2.0 in pyproject.toml and workspace
    chore: bump package version to 0.2.0 in pyproject.toml and workspace

--- src/gnssrtc/__init__.py
+++ src/gnssrtc/__init__.py
@@ -5,12 +5,16 @@
 
 # **************************************************************************************
 
+from .gps import (
+    BaseDeviceState,
+    GPSUARTDeviceInterface,
+)
 from .nmea import GPCGGNMEASentence
 from .zda import GPZDANMEASentence
 
 # **************************************************************************************
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 # **************************************************************************************
 
@@ -19,7 +23,9 @@ __license__ = "MIT"
 # **************************************************************************************
 
 __all__: list[str] = [
+    "BaseDeviceState",
     "GPCGGNMEASentence",
+    "GPSUARTDeviceInterface",
     "GPZDANMEASentence",
 ]
 
