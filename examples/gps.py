1ad0756330573e4c4d0a53a3ba48538336ab7c0f
michealroberts <michael@observerly.com>
2025-03-22 20:06:27 +0000

    chore: amend GPSUARTDeviceInterface port value in gnssrtc module
    chore: amend GPSUARTDeviceInterface port value in gnssrtc module

--- examples/gps.py
+++ examples/gps.py
@@ -11,7 +11,7 @@ from gnssrtc.gps import GPSUARTDeviceInterface
 
 # **************************************************************************************
 
-gps = GPSUARTDeviceInterface(port="/dev/ttyAMA10", baudrate=115200)
+gps = GPSUARTDeviceInterface(port="/dev/ttyS0", baudrate=115200)
 
 # **************************************************************************************
 
