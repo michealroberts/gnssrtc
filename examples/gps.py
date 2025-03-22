0202ffb829743ee864c8d6ae37b9f121a1e3a4e7
michealroberts <michael@observerly.com>
2025-03-22 20:30:10 +0000

    chore: amend GPSUARTDeviceInterface port value in gnssrtc module
    chore: amend GPSUARTDeviceInterface port value in gnssrtc module

--- examples/gps.py
+++ examples/gps.py
@@ -7,7 +7,25 @@
 
 import time
 
+from serial import Serial
+
 from gnssrtc.gps import GPSUARTDeviceInterface
+
+# **************************************************************************************
+
+ser = Serial("/dev/ttyS0", 9600, timeout=1)
+
+# **************************************************************************************
+
+# cmd1 enable GPS+BD+Galileo+QZSS:
+cmd1 = b"\x06\x3e\x3c\x00\x00\x20\x20\x07\x00\x08\x10\x00\x01\x00\x01\x01\x01\x01\x03\x00\x00\x00\x01\x01\x02\x04\x08\x00\x00\x00\x01\x01\x03\x08\x10\x00\x01\x00\x01\x01\x04\x00\x08\x00\x00\x00\x01\x03\x05\x00\x03\x00\x01\x00\x01\x05\x06\x08\x0e\x00\x00\x00\x01\x01"
+
+# cmd2 enable NMEA version 4.10 to ouput BD sentences:
+cmd2 = b"\xb5\x62\x06\x17\x14\x00\x00\x41\x00\x02\x00\x00\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00\x00"
+
+ser.write(cmd1)
+
+ser.write(cmd2)
 
 # **************************************************************************************
 
