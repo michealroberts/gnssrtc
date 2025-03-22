bcf241ce9fd2f97b5a2c57072110f090d0d01be6
michealroberts <michael@observerly.com>
2025-03-22 21:49:34 +0000

    chore: amend GPSUARTDeviceInterface port value in gnssrtc module
    chore: amend GPSUARTDeviceInterface port value in gnssrtc module

--- examples/gps.py
+++ examples/gps.py
@@ -27,6 +27,37 @@ cmd1 = b"\x06\x3e\x3c\x00\x00\x20\x20\x07\x00\x08\x10\x00\x01\x00\x01\x01\x01\x0
 # cmd2 enable NMEA version 4.10 to ouput BD sentences:
 cmd2 = b"\xb5\x62\x06\x17\x14\x00\x00\x41\x00\x02\x00\x00\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00\x00"
 
+# Please refer the file of u-blox 8 /u-blox M8 Receiver description Including protocol specification in www.u-blox.com for more info
+
+# two 8bit crc var
+ck_a = 0
+ck_b = 0
+
+
+# func of calculating the cmd1 CRC value
+def calcu_crc(cmdx):
+    global ck_a
+    global ck_b
+    ck_a = 0
+    ck_b = 0
+    for i in cmdx:
+        # print(i,' ',end='')
+        ck_a = ck_a + i
+        ck_b = ck_b + ck_a
+
+
+# calculate the the cmd1
+calcu_crc(cmd1)
+
+# combine the cmd1 bytes,like that header + conten + crc
+cmd1 = b"\xb5\x62" + cmd1 + bytes([ck_a & 0xFF, ck_b & 0xFF])
+
+# calculate the the cmd2
+calcu_crc(cmd2)
+
+# combine the cmd2 bytes,like that header + conten + crc
+cmd2 = b"\xb5\x62" + cmd2 + bytes([ck_a & 0xFF, ck_b & 0xFF])
+
 ser.write(cmd1)
 
 ser.write(cmd2)
