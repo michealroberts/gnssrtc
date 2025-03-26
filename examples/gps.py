3ccad9051b241da97193b425afc2c6d3eac570f0
michealroberts <michael@observerly.com>
2025-03-26 12:28:19 +0000

    wip: enable /dev/ttyAMA0 on GPSUARTDeviceInterface
    wip: enable /dev/ttyAMA0 on GPSUARTDeviceInterface

--- examples/gps.py
+++ examples/gps.py
@@ -11,7 +11,7 @@ from gnssrtc.gps import GPSUARTDeviceInterface
 
 # **************************************************************************************
 
-gps = GPSUARTDeviceInterface(port="/dev/ttyAMA0", baudrate=115200, timeout=1)
+gps = GPSUARTDeviceInterface(port="/dev/ttyAMA0", baudrate=9600, timeout=1)
 
 # **************************************************************************************
 
